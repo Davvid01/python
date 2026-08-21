9d178397cdc04b88aaa44f360ff6f77ddd6e59b2
Davvid01 <deberedawid@gmail.com>
2026-08-21 22:58:33 +0200

    Create Day1_task.py

---
+++ 100 Bootcamp/Day1_task.py
@@ -0,0 +1,15 @@
+#name = input("What is your name?")
+
+#print(len("Hello " + f"{name}" "!"))
+
+#print(len(input("What is your name?")))
+##
+##username=input("What is your name?")
+##length = len(username)
+##print(length)
+
+print("Welcome to the Band Name Generator\nWhat is your name of city?\n")
+miasto=input()
+
+imie=input("What is your name?: \n")
+print("Your band is "+miasto+ " " + imie)

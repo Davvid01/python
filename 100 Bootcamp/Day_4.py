3396fd0a3c429a4a174fb4ee588f5c3488eca418
Davvid01 <deberedawid@gmail.com>
2026-08-24 23:52:09 +0200

    Lists, error index

---
+++ 100 Bootcamp/Day_4.py
@@ -0,0 +1,16 @@
+fruits = ["strawberries", "apple"]
+
+vege = ["tomato", "potato"]
+
+razem = [fruits, vege]
+
+print(razem[1])
+
+dlugosc = len(fruits) #2 - 1 -> 1 bo index 0,1
+
+print(fruits[dlugosc-1])
+
+print(fruits[0][2]) # litera t
+
+
+print(razem[0][1]) # z pierwszej listy drugi string

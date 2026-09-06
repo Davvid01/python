5fd4347cf4160e2c7646057d63a491bcb8cb193d
Davvid01 <deberedawid@gmail.com>
2026-09-06 23:34:51 +0200

    Gra wisielec

--- 100 Bootcamp/Day 7/task/main.py
+++ 100 Bootcamp/Day 7/task/main.py
@@ -1,64 +1,11 @@
 import random
-word_list = ["aardvark", "baboon", "camel"]
-stages = [r'''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', r'''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', r'''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+import hangman_words
+from hangman_art import stages,logo
+
+print(logo)
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
-random_word = random.choice(word_list)
-print(random_word)
+random_word = random.choice(hangman_words.word_list)
+
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 #litera =  input("Podaj literę: ").lower()
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
@@ -70,12 +17,21 @@ lista_pomocnicza=[]
 lives=6
 while not lives<=0:
     litera =  input("Podaj literę: ").lower()
+
+    if litera in lista_pomocnicza:
+        print(f"Juz Zgadles litere {litera}")
+    elif litera not in lista_pomocnicza and litera not in random_word:
+        print(f"You guessed {litera}, that's not in the word. You lose a life")
+
+
+
     placeholder=""
     for x in random_word:
         if x==litera:
             sprawdzenie+=1
             placeholder+=litera  
             lista_pomocnicza.append(x)
+            print(lista_pomocnicza)
         elif x in lista_pomocnicza:
             placeholder+=x
         else:
@@ -83,10 +39,12 @@ while not lives<=0:
     print(placeholder) 
 
 
+
     if litera not in random_word:
         lives-=1
         if lives ==0:
             print("Game over")
+            print(f"Szukane słowo to '{random_word}'")
 
 
 
@@ -94,4 +52,5 @@ while not lives<=0:
         game_over = True
 
     print(stages[lives])
+    print(f"Zostało Tobie {lives} żyć")
 

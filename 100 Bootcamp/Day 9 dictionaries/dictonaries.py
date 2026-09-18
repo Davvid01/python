7ed1e0ec33cf67eab47665a9be89bb42a4c98f6f
Dawid De <deberedawid@gmail.com>
2026-09-18 20:25:19 +0200

    nested lists, dict

--- 100 Bootcamp/Day 9 dictionaries/dictonaries.py
+++ 100 Bootcamp/Day 9 dictionaries/dictonaries.py
@@ -8,4 +8,14 @@ programming_dictionary["Loop"] = "The action of doing something over and over"
 print(programming_dictionary)
 
 empty_dictionary = {}
-empty_dictionary[]
+print(empty_dictionary)
+
+programming_dictionary["Bug"] = "Moth in a computer"
+
+print(programming_dictionary)
+
+#loop through a dict
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
---
+++ 100 Bootcamp/Day 9 dictionaries/nesting_list_dict.py
@@ -0,0 +1,17 @@
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+# Nested List in Dictionary
+
+travel_log = {
+     "France": ["Paris", "Lille", "Dijon"],
+     "Germany": ["Stuttgart", "Berlin"],
+ }
+
+
+print(travel_log["France"][1])
+
+nested_list = ["A", "B", ["C", "D"]]
+

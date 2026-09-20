68c74204dc631958cc1e6589fd9163740e871da0
Dawid De <deberedawid@gmail.com>
2026-09-20 19:50:46 +0200

    Calculator project - functions -loops while -dictionaries

---
+++ 100 Bootcamp/Day 10 Functions with outputs/Calculator_project.py
@@ -0,0 +1,49 @@
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1,n2):
+    return n1-n2
+
+def multiply(n1,n2):
+    return n1*n2
+
+def divide(n1,n2):
+    return n1 / n2
+
+function_storage = {
+    "+" : add,
+    "-" : subtract, 
+    "*" : multiply,
+    "/" : divide
+}
+
+
+
+def calculator():
+    continue_running = True
+    number_1 = float(input("What's the first number? "))
+    while continue_running:
+        
+        operator = input("Pick an operation: ")
+        #function = function_storage[operator]
+
+        number_2 = float(input("What's the second number? "))
+
+        #result =  function(number_1 , number_2)
+        
+
+        result = function_storage[operator](number_1, number_2)
+        print(result)
+
+
+        next_calc = input(f"Type 'y' to continue calculating with {result}  or type 'n' to stop: ")
+
+        if next_calc=='y':
+            number_1=result
+        else:
+            continue_running=False
+            print("Calculation is finished")
+            break 
+
+
+calculator()
---
+++ 100 Bootcamp/Day 10 Functions with outputs/functions.py
@@ -0,0 +1,19 @@
+def format_name(f_name,l_name):
+    """Function titles provided string"""
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+
+    return f"{formatted_f_name} {formatted_l_name}"
+
+formatted_surname = format_name("aNgela","kOWalsky") 
+
+print(formatted_surname)
+
+
+def format_name(f_name, l_name):
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+
+print(format_name("AnGEla", "YU"))
